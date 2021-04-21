803c34be628798dd91a012a830f02268ad06a39e
imam <82933811+ank2307@users.noreply.github.com>
2021-04-21 18:39:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+ 	session_start();
+ 	include "koneksi.php";
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HALAMAN LOGIN</title>
+</head>
+<body>
+	<form method="post">
+		<input type="text" name="fusername"><br>
+		<label>Password :</label>
+		<input type="password" name="fpassword"><br>
+		<button type="submit" name="fmasuk">Login</button>
+		<td><a href="http://localhost/dokteronline/registrasi.php"><input type="button"value="REGISTRASI"></td></a>
+	</form>
+
+	<?php
+	if (isset($_POST['fmasuk'])) {
+	 	$username = $_POST['fusername'];
+	 	$password = $_POST['fpassword'];
+	 	$qry = mysqli_query($koneksi,"SELECT * FROM user WHERE username = '$username' AND password = '$password'");
+	 	$cek = mysqli_num_rows($qry);
+	 	if ($cek==1) {
+	 		$_SESSION['userweb']=$username;
+	 		header("location:tampilan_awal.html");
+	 		exit;
+	 	}
+	 	else{
+	 		echo "maaf username dan password ada masukan salah";
+	 	}
+	 } 
+	 ?>
+</body>
+</html>
---
+++ pengaturan.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pengaturan</title>
+</head>
+<body>
+	<div>
+		<a href="tampilan_awal.html">HOME</a>
+		
+	</div>
+	<button type="submit">lupa password</button>
+	<button type="submit">informasi</button>
+	<button type="submit">keluar</button>
+	<td><a href="http://localhost/dokteronline/index.php"><input type="button"value="KELUAR"></td></a>
+
+</body>
+</html>
